93d476c7129952db3be4d5ef7c707a3ef6aa7bc9
jerco2 <jericogodoy7296@gmail.com>
2021-12-13 15:54:35 +0800

    User model added email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
     has_secure_password 
 
     validates :email,:password_digest,:firstname,:lastname, presence: true
+    validates :email, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
 end
