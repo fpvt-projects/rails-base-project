74a3e5e5a79a95a483baaa3c481af3dce142dbdf
jerco2 <jericogodoy7296@gmail.com>
2021-12-14 16:33:30 +0800

    Insalled ReactJS,react-router, & styled components. Route Root, Login, & Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
   get "Home", to: "home#index"
+  get '*path', to: 'home#index'
   
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
